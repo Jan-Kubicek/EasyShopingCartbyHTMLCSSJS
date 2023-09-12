88d9b240522f58e241b82f07d139364c6ddd93fd
Jan Kubicek <143211313+Jan-Kubicek@users.noreply.github.com>
2023-09-12 16:27:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,26 @@ function good(name, numberOfAvaiablePieces, pricePerPiece){
     this.numberOfAvaiablePieces = numberOfAvaiablePieces;
     this.pricePerPiece = pricePerPiece;
 }
+
 function receipt(){
     let mena ;
     let sum ;
     aler(/* return receipt sum + mena type + all selectedGoods + sum of selectedGoods */);
+}
+
+function importGoods(){
+    let table = document.getElementById("table-goods");
+    let row = table.insertRow(-1);
+    let cellID = row.insertCell(0);
+    cellID.innerHTML = "ID";
+    let cellName = row.insertCell(1);
+    cellName.innerHTML = "Name";
+    let cellNumberOfAvaiablePieces = row.insertCell(2);
+    cellNumberOfAvaiablePieces.innerHTML = "Number";
+    let cellPricePerPiece = row.insertCell(3);
+    cellPricePerPiece.innerHTML = "5$";
+}
+
+function reset(){
+    importGoods();
 }
