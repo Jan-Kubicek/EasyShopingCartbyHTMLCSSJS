5c0c3c86e381fcb67e33be493efa19d9e3eec91c
Jan Kubicek <143211313+Jan-Kubicek@users.noreply.github.com>
2023-09-12 15:38:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const goods = [];
+const selectedGoods = [];
+
+
+function good(name, numberOfAvaiablePieces, pricePerPiece){
+    this.name = name; 
+    this.numberOfAvaiablePieces = numberOfAvaiablePieces;
+    this.pricePerPiece = pricePerPiece;
+}
+function receipt(){
+    let mena ;
+    let sum ;
+    aler(/* return receipt sum + mena type + all selectedGoods + sum of selectedGoods */);
+}
